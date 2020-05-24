397e39ebe3e29f547bd10e64375d1c286ff2da4e
ryansimpsonuk <65786721+ryansimpsonuk@users.noreply.github.com>
2020-05-24 13:14:53 +0100

    first cut - just laying out my thoughts

---
+++ notebooks/Research/01-Tutorial.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC Some very cool applications here: https://github.com/nashory/gans-awesome-applications
+# MAGIC 
+# MAGIC Following sample taken from https://medium.com/ai-society/gans-from-scratch-1-a-deep-introduction-with-code-in-pytorch-and-tensorflow-cb03cdcdba0f
+
+# COMMAND ----------
+
